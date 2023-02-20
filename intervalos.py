a8d0a8c76a3d6eb36e31567792be5179653de4af
SugaryLump <sugarcanegaming6000@gmail.com>
2023-02-20 18:17:56 +0000

    Interval parser

---
+++ intervalos.py
@@ -0,0 +1,65 @@
+import ply.lex as lex
+import ply.yacc as yacc
+
+tokens = (
+    'PLUS', 'MINUS', 'COMMA',
+    'NUMBER', 'OPEN', 'CLOSE',
+)
+
+t_PLUS      = r'\+'
+t_MINUS     = r'-'
+t_COMMA     = r','
+t_OPEN      = r'\['
+t_CLOSE     = r'\]'
+t_NUMBER    = r'\-?\d+'
+
+t_ignore    = ' '
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+lexer = lex.lex()
+
+mode = None
+intervals = []
+def p_mode(t):
+    '''mode : PLUS intervals
+            | MINUS intervals'''
+    intervals.reverse()
+    for i in range(0, len(intervals)):
+        if i > 0:
+            if t[1] == '-' and intervals[i-1][0] <= intervals[i][1]:
+                raise Exception("Error: Intervals are not in sequence " + str(intervals[i-1]) + " -> " + str(intervals[i]))
+            elif t[1] == '+' and intervals[i-1][1] >= intervals[i][0]:
+                raise Exception("Error: Intervals are not in sequence " + str(intervals[i-1]) + " -> " + str(intervals[i]))
+
+def p_intervals(t):
+    '''intervals : interval intervals
+                 | interval'''
+    intervals.append(t[1])
+
+def p_interval(t):
+    'interval : OPEN NUMBER COMMA NUMBER CLOSE'
+    t[0] = [int(t[2]), int(t[4])]
+    
+    if t[2] >= t[4]:
+        raise Exception("Error: Interval " + str(t[0]) + " is invalid.")
+
+def p_error(t):
+    print("Syntax error at '%s'" % t.value)
+
+parser = yacc.yacc()
+
+while True:
+    mode = None
+    intervals = []
+    try:
+        s = input('$ ')
+    except EOFError as e:
+        print(e)
+        break
+    try:
+        parser.parse(s)
+    except Exception as e:
+        print(e)
